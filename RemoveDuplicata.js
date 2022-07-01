1a6089c574a943930f1407d8d6d79608da2f36da
Eric-Catarina <102993694+Eric-Catarina@users.noreply.github.com>
2022-07-01 11:45:56 -0300

    Stable 😎

--- RemoveDuplicata.js
+++ RemoveDuplicata.js
@@ -4,71 +4,86 @@ var rl = readline.createInterface({
     output: process.stdout,
     terminal: false
 });
+
 rl.on('line', entrada => {
-    // Primeiro confere se é ímpar --> se for, tira a primeira letra, divide no meio e vai coisando: 
-    // se a primeira metade for igual a segunda return T, 
-    // else {Tira as duas primeiras letras que sobraram e testa de novo }..
-
-    var inicio = 0
-    var numeroDeChamadas = 0
-
-    let verificaSeStringTemTamanhoImpar = (stringAlvo) => stringAlvo.length % 2 != 0
-    let verificaSeStringEhDigrafo = (stringAlvo) => stringAlvo.length == 2
-    let verificaSeDigrafoRepete =  (digrafoAlvo) => {
-        let stringAlvoVetor = digrafoAlvo.split("")
-        if (stringAlvoVetor[0] == stringAlvoVetor[1]) {
-            return true
-        }
-        else {
-            return false
-        }
-    }
-    let verificaSeEhDigrafoESeRepete = (entrada) =>{
-        if (verificaSeStringEhDigrafo(entrada)) {
-            return verificaSeDigrafoRepete (entrada)
-        }
-        return false
-    }
 
     let entradaSeparadaPorPalavras = entrada.split(" ")
-    /*if (verificaSeStringTemTamanhoImpar(stringAlvo)) {
-        inicio = 1
-    }*/
+
+    let vetorResposta = []
+
+    let resposta
+
+    let MetadeTermoUm
+    let MetadeTermoDois
+
+    let stringInicial
+
+    let verificaSeStringTemTamanhoImpar = (stringAlvo) => stringAlvo.length % 2 != 0
+
+    let verificaSeTemDuplicata = (stringAlvo) => {
+        stringInicial = stringAlvo
+        if (verificaSeStringTemTamanhoImpar(stringAlvo)) {
+            stringAlvo = stringAlvo.slice(1)
+        }
+        return verificaSeTemDuplicataRecursiva(stringAlvo)
+    }
 
     let verificaSeTemDuplicataRecursiva = (stringAlvo) => {
+        let tamanhoDaParteDuplicada
 
-        let MetadeTermoUm = stringAlvo.slice(0, stringAlvo.length / 2)
-        let MetadeTermoDois = stringAlvo.slice(stringAlvo.length / 2)
-        console.log (MetadeTermoUm + ";" + MetadeTermoDois)
-        
-        /*
-        t|or|cidaCida
-        0|12|345678910
-        */
-        if (MetadeTermoUm  == "" || MetadeTermoDois ==""){
+        MetadeTermoUm = stringAlvo.slice(0, stringAlvo.length / 2)
+        MetadeTermoDois = stringAlvo.slice(stringAlvo.length / 2)
+
+        if (MetadeTermoUm == "" || MetadeTermoDois == "") {
             return false
         }
         if (MetadeTermoUm == MetadeTermoDois) {
+
+            tamanhoDaParteDuplicada = MetadeTermoDois.length
+            vetorResposta.push(stringInicial.slice(0, -tamanhoDaParteDuplicada))
+
             return true
         }
         else {
-            
             stringAlvo = stringAlvo.slice(2)
             return verificaSeTemDuplicataRecursiva(stringAlvo)
         }
     }
-    /*
-    resposta = MetadeTermoUm + ";" + MetadeTermoDois
-        return true
-    */
-    // Escreva a sua solução aqui
-    let verificaSeTemDuplicata = (entrada) =>{
-        if (verificaSeStringTemTamanhoImpar(entrada)){
-            entrada = entrada.slice(1)
+
+    let verificaSeTodasPalavrasSaoDuplicatas = (vetorStringAlvo) => {
+        var contadorPalavrasDuplicadas = 0
+
+        vetorStringAlvo.forEach(palavra => {
+            if (verificaSeTemDuplicata(palavra)) {
+                contadorPalavrasDuplicadas++
+            }
+        })
+        if (contadorPalavrasDuplicadas == vetorStringAlvo.length) {
+            return true
         }
-        return verificaSeTemDuplicataRecursiva(entrada)
+        else {
+            return false
+        }
+    }
+
+    let converteVetorEmString = (vetorAlvo) => {
+
+        let stringFinal
+
+        stringFinal = vetorAlvo.join(" ")
+
+        return stringFinal + "."
     }
     
-    console.log(verificaSeTemDuplicata(entrada))
+    // Escreva a sua solução aqui
+    
+    if (verificaSeTodasPalavrasSaoDuplicatas(entradaSeparadaPorPalavras)){
+        resposta = converteVetorEmString(vetorResposta)
+    }
+    else{
+        resposta = entrada
+    }
+    console.log(resposta)
+
 
 })
